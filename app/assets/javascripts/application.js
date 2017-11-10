c5ba797c1d61fedb7a7a61ada2f42fd7e99d471f
Christi Ovchinnikov Barker <kaco7777@gmail.com>
2017-11-09 20:28:25 -0700

    html templates

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,9 @@
 // about supported directives.
 //
 
-//=require jquery
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
 //=require bootstrap
 //= require rails-ujs
 //= require turbolinks
